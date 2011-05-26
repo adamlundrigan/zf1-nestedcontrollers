6b47a9c4e58f79ac8a3b36babe841003afb2b0a0
Adam Lundrigan <adamlundrigan@cdli.ca>
2011-05-26 21:10:14 -0230

    Started fresh

---
+++ library/CDLI/Standard/Mvc/Resource/Subcontrollers.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * CDLI Standard Library for Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   CDLI
+ * @package    Standard
+ * @subpackage Mvc
+ * @copyright  Copyright (c) 2011 Government of Newfoundland and Labrador
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @version    $Id$
+ */
+
+/**
+ * @see Zend_Application_Resource_ResourceAbstract
+ */
+require_once 'Zend/Application/Resource/ResourceAbstract.php';
+
+
+/**
+ * Application Resource to manage MVC routing to subcontrollers
+ *
+ * @category   CDLI
+ * @package    Standard
+ * @subpackage Mvc
+ * @author Adam Lundrigan <adam@lundrigan.ca>
+ * @copyright  Copyright (c) 2011 Government of Newfoundland and Labrador
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class CDLI_Standard_Mvc_Resource_Subcontrollers extends Zend_Application_Resource_ResourceAbstract
+{
+    
+}
