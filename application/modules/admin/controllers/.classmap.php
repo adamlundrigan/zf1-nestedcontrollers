9a2a4d0ca03bebd307c5f475bcbcc55e4f32a9f9
Adam Lundrigan <adamlundrigan@cdli.ca>
2011-05-26 22:34:30 -0230

    Created admin test module

---
+++ application/modules/admin/controllers/.classmap.php
@@ -0,0 +1,6 @@
+<?php
+$dirname_4ddef7d3533a4 = dirname(__FILE__);
+return array (
+  'Admin_Page_SubPage_DisplayController' => $dirname_4ddef7d3533a4 . DIRECTORY_SEPARATOR . 'Page' . DIRECTORY_SEPARATOR . 'SubPage' . DIRECTORY_SEPARATOR . 'DisplayController.php',
+  'Admin_Page_DisplayController' => $dirname_4ddef7d3533a4 . DIRECTORY_SEPARATOR . 'Page' . DIRECTORY_SEPARATOR . 'DisplayController.php',
+);
