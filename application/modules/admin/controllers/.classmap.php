546aaaf17a1d3c94a92ed03b16cfa6a2f2763cbf
Adam Lundrigan <adamlundrigan@cdli.ca>
2011-05-27 10:37:11 -0230

    Updated classmap file for admin module

--- application/modules/admin/controllers/.classmap.php
+++ application/modules/admin/controllers/.classmap.php
@@ -1,6 +1,7 @@
 <?php
-$dirname_4ddef7d3533a4 = dirname(__FILE__);
+$dirname_4ddfa1d65bbdc = dirname(__FILE__);
 return array (
-  'Admin_Page_SubPage_DisplayController' => $dirname_4ddef7d3533a4 . DIRECTORY_SEPARATOR . 'Page' . DIRECTORY_SEPARATOR . 'SubPage' . DIRECTORY_SEPARATOR . 'DisplayController.php',
-  'Admin_Page_DisplayController' => $dirname_4ddef7d3533a4 . DIRECTORY_SEPARATOR . 'Page' . DIRECTORY_SEPARATOR . 'DisplayController.php',
+  'Admin_StandardController' => $dirname_4ddfa1d65bbdc . DIRECTORY_SEPARATOR . 'StandardController.php',
+  'Admin_Page_SubPage_DisplayController' => $dirname_4ddfa1d65bbdc . DIRECTORY_SEPARATOR . 'Page' . DIRECTORY_SEPARATOR . 'SubPage' . DIRECTORY_SEPARATOR . 'DisplayController.php',
+  'Admin_Page_DisplayController' => $dirname_4ddfa1d65bbdc . DIRECTORY_SEPARATOR . 'Page' . DIRECTORY_SEPARATOR . 'DisplayController.php',
 );
