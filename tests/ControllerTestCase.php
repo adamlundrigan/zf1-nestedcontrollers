494e3b7052d2cf99e378b2c32d70e6c8b77e14d8
Adam Lundrigan <adamlundrigan@cdli.ca>
2011-05-30 23:40:39 -0230

    Simplified ControllerTestCase

--- tests/ControllerTestCase.php
+++ tests/ControllerTestCase.php
@@ -6,23 +6,14 @@ require_once 'Zend/Application.php';
  */
 abstract class ControllerTestCase extends Zend_Test_PHPUnit_ControllerTestCase
 {
-    public $application;
 
     public function setUp()
     {
-
-        $this->application = new Zend_Application(
+        $this->bootstrap = new Zend_Application(
             APPLICATION_ENV,
             realpath(APPLICATION_PATH . '/configs/application.ini')
         );
-
-        $this->bootstrap = array($this, 'appBootstrap');
         parent::setUp();
-    }
-
-    public function appBootstrap()
-    {
-        $this->application->bootstrap();
     }
     
 }
